99518aa5d44c28ae85ba2fdfa423fe3914da7164
Mauro Vieira <55004921+Malvon@users.noreply.github.com>
2022-05-26 19:13:27 -0300

    Update Desafio081.py

--- Mundo 3/Aula 17 – Listas (Parte 1)/Desafio081.py
+++ Mundo 3/Aula 17 – Listas (Parte 1)/Desafio081.py
@@ -23,29 +23,3 @@ while True:
         else:
             print('O valor 5 não faz parte da lista!')
         break
-# #
-# # passo 1 criar lista
-# valores = []
-# cont = 0
-# # passo 2 incluri valores
-# while True:
-#     valores.append(int(input('Digite um valor: ')))
-#     cont += 1
-#     resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-#     if resp == 'S':
-#         continue
-#     else:
-#         break
-# # passo 3 saber quantos foram dig
-# print(f'Voce digitou {cont} valores')
-#
-# # passo 4 ordenar lista de forma decescente
-# valores.sort(reverse=True)
-# print(f'Os valores em ordem decescente sao {valores}')
-#
-#
-# # passo 5 se valor 5 foi dig e esta ou nao na list
-# if 5 in valores:
-#     print('O valor 5 faz parte da lista!')
-# else:
-#     print('O valor 5 nao faz parte da lista!')
